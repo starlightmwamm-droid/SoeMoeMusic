59a683258578ddb04d53372250c8acde20bfa8bf
SoeMoe <starlight.mwa.mm@gmail.com>
2026-07-06 21:17:16 +0630

    Update config.py

--- config.py
+++ config.py
@@ -78,7 +78,7 @@ class Config:
         self.VIDEO_MAX_HEIGHT: int = self._parse_video_height()
 
         # ArtistBots API
-        self.ARTISTBOTS_API_URL: str = getenv("ARTISTBOTS_API_URL", "https://artistbotsapi.onrender.com")
+        self.ARTISTBOTS_API_URL: str = getenv("ARTISTBOTS_API_URL", "http://Artistbotsapi.onrender.com")
         self.ARTISTBOTS_KEY: str = getenv("ARTISTBOTS_KEY", "")
         self.ENABLE_API: bool = self._str_to_bool(getenv("ENABLE_API", "True"))
         self.ENABLE_COOKIES_FALLBACK: bool = self._str_to_bool(getenv("ENABLE_COOKIES_FALLBACK", "True"))
